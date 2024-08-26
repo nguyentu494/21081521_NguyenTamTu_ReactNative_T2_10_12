1e4de49e1bc76e5b9bb1409c06b8cf77213a068f
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2024-08-26 16:51:59 +0700

    commit fix bai3_P2

--- Buoi2_Ngay26_08/JSFunda_Part2/bai3_P2.js
+++ Buoi2_Ngay26_08/JSFunda_Part2/bai3_P2.js
@@ -2,22 +2,23 @@
 let objMark = {
     fullName: 'Mark Miller',
     mass: 78,
-    height: 1.69
+    height: 1.69,
+    calcBMI: function()  {
+        this.BMI = Math.floor(this.mass / Math.pow(this.height, 2))
+        return this.BMI;
+    }
 }
 
 let objJohn = {
     fullName: 'John Smith',
     mass: 92,
-    height: 1.95
+    height: 1.95,
+    calcBMI: objMark.calcBMI
 }
 
 // 2.3.2
-let calcBMI = (person) => {
-    person.BMI = Math.floor(person.mass / Math.pow(person.height, 2))
-}
-
-calcBMI(objMark)
-calcBMI(objJohn)
+objJohn.calcBMI()
+objMark.calcBMI()
 
 // 2.3.3
 console.log(objMark.BMI>objJohn.BMI?`${objMark.fullName} BMI (${objMark.BMI}) is higher than ${objJohn.fullName} (${objJohn.BMI})!`
