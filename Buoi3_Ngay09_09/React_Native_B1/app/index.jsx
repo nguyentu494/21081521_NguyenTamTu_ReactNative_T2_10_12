39798ef9f1347cd469d209ea2bdea5feba3a7869
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2024-10-04 11:02:47 +0700

    fix lab1_RN

--- Buoi3_Ngay09_09/React_Native_B1/app/index.jsx
+++ Buoi3_Ngay09_09/React_Native_B1/app/index.jsx
@@ -6,14 +6,14 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View>
-      <Link href="/Screen1">Link 1</Link>
-      <Link href="/Screen2">Link 2</Link>
-      <Link href="/Screen3">Link 3</Link>
-      <Link href="/Screen4">Link 4</Link>
-      <Link href="/Screen5">Link 5</Link>
-      <Link href="/Screen6">Link 6</Link>
-      <Link href="/Screen7">Link 7</Link>
-      <Link href="/Screen8">Link 8</Link>
+      <Link href="/Screen1">Screen 1</Link>
+      <Link href="/Screen2">Screen 2</Link>
+      <Link href="/Screen3">Screen 3</Link>
+      <Link href="/Screen4">Screen 4</Link>
+      <Link href="/Screen5">Screen 5</Link>
+      <Link href="/Screen6">Screen 6</Link>
+      <Link href="/Screen7">Screen 7</Link>
+      <Link href="/Screen8">Screen 8</Link>
     </View>
   );
 }
